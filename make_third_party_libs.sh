6136b68aeef84f539af5813d2da679621fb9fde1
Robert Hilton <robert.a.hilton.jr@gmail.com>
2022-01-19 19:57:16 -0500

    Add make for third party libs

---
+++ make_third_party_libs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+forall() {
+	COMMAND=$@
+	if [ ${#@} -lt 1 ]; then
+    	echo "No command entered! Please enter a command to run!"
+		return;
+	fi
+	echo "Running $COMMAND"
+	find . -maxdepth 1 \( ! -name . \) | grep -v \\./\\. | xargs -I {} bash -c "cd '{}' && echo && echo {} && $COMMAND";
+}
+
+forall make
