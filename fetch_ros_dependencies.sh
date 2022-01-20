07c3ef0f5cfdfd0196e95fc5d16278bc40c2e8b1
Robert <robert.a.hilton.jr@gmail.com>
2022-01-19 23:20:04 -0500

    Update fetch_ros_dependencies.sh

--- fetch_ros_dependencies.sh
+++ fetch_ros_dependencies.sh
@@ -6,14 +6,16 @@ export SKIP_LIST=$(curl -sL https://raw.githubusercontent.com/frcteam195/ros-ci-
 clone_dependency()
 {
 	if [ $# -gt 0 ]; then
-		if [[ "${SKIP_FILE}" == "" ]]; then
-			if [[ ! $(echo "${SKIP_LIST}" | grep -F "${1}") ]]; then
-				git clone --recurse-submodules -j4 "https://github.com/frcteam195/${1}.git"
+		if [[ -f "${1}" ]]; then
+			if [[ "${SKIP_FILE}" == "" ]]; then
+				if [[ ! $(echo "${SKIP_LIST}" | grep -F "${1}") ]]; then
+					git clone --recurse-submodules -j4 "https://github.com/frcteam195/${1}.git"
+				fi
+			else
+				if [[ ! $(grep -F "${1}" "${SKIP_FILE}") ]]; then
+					git clone --recurse-submodules -j4 "https://github.com/frcteam195/${1}.git"
+				fi
 			fi
-		else
-			if [[ ! $(grep -F "${1}" "${SKIP_FILE}") ]]; then
-                                git clone --recurse-submodules -j4 "https://github.com/frcteam195/${1}.git"
-                        fi
 		fi
 	fi
 }
