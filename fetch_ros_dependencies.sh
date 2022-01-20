bf131383278ab150176277f8b720bdf9787a40d6
Robert <robert.a.hilton.jr@gmail.com>
2022-01-19 23:50:17 -0500

    Update fetch_ros_dependencies.sh

--- fetch_ros_dependencies.sh
+++ fetch_ros_dependencies.sh
@@ -6,24 +6,23 @@ export SKIP_LIST=$(curl -sL https://raw.githubusercontent.com/frcteam195/ros-ci-
 clone_dependency()
 {
 	if [ $# -gt 0 ]; then
-		if [[ -f "${1}" ]]; then
-			if [[ "${SKIP_FILE}" == "" ]]; then
-				if [[ ! $(echo "${SKIP_LIST}" | grep -F "${1}") ]]; then
-					if [[ ! -d "${1}" ]]; then
-						git clone --recurse-submodules -j4 "https://github.com/frcteam195/${1}.git"
-					fi
+		if [[ "${SKIP_FILE}" == "" ]]; then
+			if [[ ! $(echo "${SKIP_LIST}" | grep -F "${1}") ]]; then
+				if [[ ! -d "${1}" ]]; then
+					git clone --recurse-submodules -j4 "https://github.com/frcteam195/${1}.git"
 				fi
-			else
-				if [[ ! $(grep -F "${1}" "${SKIP_FILE}") ]]; then
-					if [[ ! -d "${1}" ]]; then
-						git clone --recurse-submodules -j4 "https://github.com/frcteam195/${1}.git"
-					fi
+			fi
+		else
+			if [[ ! $(grep -F "${1}" "${SKIP_FILE}") ]]; then
+				if [[ ! -d "${1}" ]]; then
+					git clone --recurse-submodules -j4 "https://github.com/frcteam195/${1}.git"
 				fi
 			fi
 		fi
 	fi
 }
 export -f clone_dependency
-
-xmllint --xpath "//package/build_depend/text()" "${PACKAGE_XML}" | xargs -P 4 -I {} bash -c "clone_dependency {}"
-xmllint --xpath "//package/depend/text()" "${PACKAGE_XML}" | xargs -P 4 -I {} bash -c "clone_dependency {}"
+if [[ -f "${1}" ]]; then
+	xmllint --xpath "//package/build_depend/text()" "${PACKAGE_XML}" | xargs -P 4 -I {} bash -c "clone_dependency {}"
+	xmllint --xpath "//package/depend/text()" "${PACKAGE_XML}" | xargs -P 4 -I {} bash -c "clone_dependency {}"
+fi
